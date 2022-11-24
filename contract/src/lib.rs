6f73603308af3157aa886ca8457d8f59803fc455
Alexander Sakhaev <alsakhaev@gmail.com>
2022-11-24 19:16:24 -0300

    verify personal_sign

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,84 +1,45 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
-
+use near_sdk::{near_bindgen, env};
+use near_sdk::serde::{Serialize, Deserialize};
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Counter {
-    val: i8,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct EcrecoverInput {
+    #[serde(with = "hex::serde")]
+    m: [u8; 32],
+    v: u8,
+    #[serde(with = "hex::serde")]
+    sig: [u8; 64],
+    mc: bool,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct EcrecoverOutput {
+    #[serde(with = "hex::serde")]
+    address: [u8; 20]
 }
 
 #[near_bindgen]
 impl Counter {
-    // Public read-only method: Returns the counter value.
-    pub fn get_num(&self) -> i8 {
-        return self.val;
-    }
-
-    // Public method: Increment the counter.
-    pub fn increment(&mut self) {
-        self.val += 1;
-        log!("Increased number to {}", self.val);
-    }
-
-    // Public method: Decrement the counter.
-    pub fn decrement(&mut self) {
-        self.val -= 1;
-        log!("Decreased number to {}", self.val);
-    }
-
-    // Public method - Reset to zero.
-    pub fn reset(&mut self) {
-        self.val = 0;
-        log!("Reset counter to zero");
+    pub fn eth_ecrecover(&self, data: EcrecoverInput) -> EcrecoverOutput {
+        match env::ecrecover(&data.m, &data.sig, data.v, data.mc) {
+            Some(pubkey) => {
+                let hash = env::keccak256_array(&pubkey);
+                let address: [u8; 20] = hash[12..32].try_into().expect("Incorrect length"); // take last 20 bytes
+                EcrecoverOutput { address }
+            },
+            None => EcrecoverOutput { address: [0; 20] },
+        }
     }
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be: `cargo test`
- * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn increment() {
-        // instantiate a contract variable with the counter at zero
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        assert_eq!(1, contract.get_num());
-    }
-
-    #[test]
-    fn decrement() {
-        let mut contract = Counter { val: 0 };
-        contract.decrement();
-        assert_eq!(-1, contract.get_num());
-    }
-
-    #[test]
-    fn increment_and_reset() {
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        contract.reset();
-        assert_eq!(0, contract.get_num());
-    }
-
-    #[test]
-    #[should_panic]
-    fn panics_on_overflow() {
-        let mut contract = Counter { val: 127 };
-        contract.increment();
-    }
-
-    #[test]
-    #[should_panic]
-    fn panics_on_underflow() {
-        let mut contract = Counter { val: -128 };
-        contract.decrement();
-    }
 }
--- integration-tests/src/main.ava.ts
+++ integration-tests/src/main.ava.ts
@@ -1,5 +1,6 @@
 import { Worker, NEAR, NearAccount } from "near-workspaces";
 import anyTest, { TestFn } from "ava";
+import * as ethers from "ethers";
 
 const test = anyTest as TestFn<{
   worker: Worker;
@@ -34,28 +35,83 @@ test.afterEach(async (t) => {
   });
 });
 
-test("can be incremented", async (t) => {
-  const { alice, contract } = t.context.accounts;
-  const startCounter: number = await contract.view("get_num", {});
-  await alice.call(contract, "increment", {});
-  const endCounter = await contract.view("get_num", {});
-  t.is(endCounter, startCounter + 1);
-});
+test("calculates ecrecover for personal_sign", async (t) => {
+  // ARRANGE =======================
 
-test("can be decremented", async (t) => {
-  const { alice, contract } = t.context.accounts;
-  await alice.call(contract, "increment", {});
-  const startCounter: number = await contract.view("get_num", {});
-  await alice.call(contract, "decrement", {});
-  const endCounter = await contract.view("get_num", {});
-  t.is(endCounter, startCounter - 1);
-});
+  const message = "Hello, World!";
 
-test("can be reset", async (t) => {
-  const { alice, contract } = t.context.accounts;
-  await alice.call(contract, "increment", {});
-  await alice.call(contract, "increment", {});
-  await alice.call(contract, "reset", {});
-  const endCounter = await contract.view("get_num", {});
-  t.is(endCounter, 0);
+  const wallet = ethers.Wallet.createRandom();
+  const signature = await wallet.signMessage(message);
+
+  const prefixedMessage = "\x19Ethereum Signed Message:\n" + message.length + message;
+  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66); // remove 0x
+
+  const sig = signature.slice(2, 130); // first 64 bytes without 0x
+  const v = signature.slice(130, 132); // last 1 byte
+
+  // Transform yellow paper V from 27/28 to 0/1
+  // More info:
+  // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
+  // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
+  const compatibleV = parseInt('0x' + v) - 27;
+
+  const expectedAddress = wallet.address.toLowerCase();
+
+  // ACT ============================
+
+  const { contract } = t.context.accounts;
+  const result: any = await contract.view("eth_ecrecover", {
+    data: {
+      m: hash,
+      sig: sig,
+      v: compatibleV,
+      mc: false // ToDo: check correctness
+    }
+  });
+
+  const receivedAddress = '0x' + result.address.toLowerCase();
+
+  // ASSERT =========================
+
+  t.is(receivedAddress, expectedAddress);
+})
+
+test("calculates ecrecover for given signature", async (t) => {
+  // ARRANGE =======================
+
+  const message = "Example `personal_sign` message";
+  const signature = "0xb6483ed445dbd979650106011bc1c6e6ef255c84404df7729bdaf8bda1ae428b19c13e1447bc3fdf618b351c951d992ee236b462dc3e8f9a6fd8dc952aa18ebe1b";
+  const address = "0xaAF9E9Ce86D3f85ee15797B996c33eB720b185c0";
+  
+  const prefixedMessage = "\x19Ethereum Signed Message:\n" + message.length + message;
+  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66); // remove 0x
+
+  const sig = signature.slice(2, 130); // first 64 bytes without 0x
+  const v = signature.slice(130, 132); // last 1 byte
+
+  // Transform yellow paper V from 27/28 to 0/1
+  // More info:
+  // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
+  // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
+  const compatibleV = parseInt('0x' + v) - 27;
+  
+  const expectedAddress = address.toLowerCase();
+
+  // ACT ============================
+
+  const { contract } = t.context.accounts;
+  const result: any = await contract.view("eth_ecrecover", {
+    data: {
+      m: hash,
+      sig: sig,
+      v: compatibleV,
+      mc: false // ToDo: check correctness
+    }
+  });
+
+  const receivedAddress = '0x' + result.address.toLowerCase();
+
+  // ASSERT =========================
+
+  t.is(receivedAddress, expectedAddress);
 });
