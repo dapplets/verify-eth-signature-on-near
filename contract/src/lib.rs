df1a4dcb5c4b24a4066b9166da526dec2413b262
Nikita Teremovskiy <ni-2@mail.ru>
2023-01-25 15:34:18 +0300

    feat: add statement to LinkingAccounts structure

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,13 +1,12 @@
+use ethabi::token::Token;
+use ethabi::{encode, Address, Uint};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env};
-use near_sdk::serde::{Serialize, Deserialize};
-use ethabi::token::{Token};
-use ethabi::{encode,Uint,Address};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen};
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Counter {
-}
+pub struct Counter {}
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
@@ -24,7 +23,7 @@ pub struct EcrecoverInput {
 #[serde(crate = "near_sdk::serde")]
 pub struct EcrecoverOutput {
     #[serde(with = "hex::serde")]
-    address: [u8; 20]
+    address: [u8; 20],
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
@@ -48,35 +47,43 @@ pub struct LinkingAccount {
 pub struct LinkingAccounts {
     account_a: LinkingAccount,
     account_b: LinkingAccount,
+    statement: String,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct HashOutput {
     #[serde(with = "hex::serde")]
-    hash: [u8; 32]
+    hash: [u8; 32],
 }
 
 fn _domain_separator() -> [u8; 32] {
     let chain_id = 5;
-    let verifying_contract = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
-    let eip712_domain = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+    let verifying_contract = [
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00,
+    ];
+    let eip712_domain =
+        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
     let name = "Connected Accounts";
     let version = "1";
     let eip712_domain_typehash = env::keccak256_array(&eip712_domain.as_bytes());
-    let domain_separator = env::keccak256_array(encode(&[
-        Token::FixedBytes(eip712_domain_typehash.to_vec()),
-        Token::FixedBytes(env::keccak256_array(&name.as_bytes()).to_vec()),
-        Token::FixedBytes(env::keccak256_array(&version.as_bytes()).to_vec()),
-        Token::Uint(Uint::from(chain_id)),
-        Token::Address(Address::from(verifying_contract)),
-    ]).as_slice());
+    let domain_separator = env::keccak256_array(
+        encode(&[
+            Token::FixedBytes(eip712_domain_typehash.to_vec()),
+            Token::FixedBytes(env::keccak256_array(&name.as_bytes()).to_vec()),
+            Token::FixedBytes(env::keccak256_array(&version.as_bytes()).to_vec()),
+            Token::Uint(Uint::from(chain_id)),
+            Token::Address(Address::from(verifying_contract)),
+        ])
+        .as_slice(),
+    );
 
     domain_separator
 }
 
 fn _hash_linking_accounts_no_domain(linking_accounts: LinkingAccounts) -> [u8; 32] {
-    let linking_accounts_type = "LinkingAccounts(LinkingAccount account_a,LinkingAccount account_b)LinkingAccount(string origin_id,string account_id)";
+    let linking_accounts_type = "LinkingAccounts(LinkingAccount account_a,LinkingAccount account_b,string statement)LinkingAccount(string origin_id,string account_id)";
     let linking_accounts_typehash = env::keccak256_array(&linking_accounts_type.as_bytes());
 
     env::keccak256_array(
@@ -84,7 +91,9 @@ fn _hash_linking_accounts_no_domain(linking_accounts: LinkingAccounts) -> [u8; 3
             Token::FixedBytes(linking_accounts_typehash.to_vec()),
             Token::FixedBytes(_hash_linking_account(linking_accounts.account_a).to_vec()),
             Token::FixedBytes(_hash_linking_account(linking_accounts.account_b).to_vec()),
-        ]).as_slice()
+            Token::FixedBytes(env::keccak256_array(linking_accounts.statement.as_bytes()).to_vec()),
+        ])
+        .as_slice(),
     )
 }
 
@@ -95,7 +104,7 @@ fn _hash_linking_accounts(linking_accounts: LinkingAccounts) -> [u8; 32] {
     let prefix = [0x19, 0x01].to_vec(); // "\\x19\\x01"
     let postfix = encode(&[
         Token::FixedBytes(domain_separator.to_vec()),
-        Token::FixedBytes(hash_linking_accounts_no_domain.to_vec())
+        Token::FixedBytes(hash_linking_accounts_no_domain.to_vec()),
     ]);
     let concated = [prefix, postfix].concat();
 
@@ -106,11 +115,14 @@ fn _hash_linking_account(linking_account: LinkingAccount) -> [u8; 32] {
     let linking_account_type = "LinkingAccount(string origin_id,string account_id)";
     let linking_account_typehash = env::keccak256_array(&linking_account_type.as_bytes());
 
-    env::keccak256_array(encode(&[
-        Token::FixedBytes(linking_account_typehash.to_vec()),
-        Token::FixedBytes(env::keccak256_array(linking_account.origin_id.as_bytes()).to_vec()), // strings must be hashed
-        Token::FixedBytes(env::keccak256_array(linking_account.account_id.as_bytes()).to_vec()), // strings must be hashed
-    ]).as_slice())
+    env::keccak256_array(
+        encode(&[
+            Token::FixedBytes(linking_account_typehash.to_vec()),
+            Token::FixedBytes(env::keccak256_array(linking_account.origin_id.as_bytes()).to_vec()), // strings must be hashed
+            Token::FixedBytes(env::keccak256_array(linking_account.account_id.as_bytes()).to_vec()), // strings must be hashed
+        ])
+        .as_slice(),
+    )
 }
 
 #[near_bindgen]
@@ -121,19 +133,23 @@ impl Counter {
                 let hash = env::keccak256_array(&pubkey);
                 let address: [u8; 20] = hash[12..32].try_into().expect("Incorrect length"); // take last 20 bytes
                 EcrecoverOutput { address }
-            },
+            }
             None => EcrecoverOutput { address: [0; 20] },
         }
     }
 
-    pub fn eth_verify_eip712(&self, linking_accounts: LinkingAccounts, signature: EcrecoverSigInput) -> EcrecoverOutput {
+    pub fn eth_verify_eip712(
+        &self,
+        linking_accounts: LinkingAccounts,
+        signature: EcrecoverSigInput,
+    ) -> EcrecoverOutput {
         let hash = _hash_linking_accounts(linking_accounts);
         match env::ecrecover(&hash, &signature.sig, signature.v, signature.mc) {
             Some(pubkey) => {
                 let hash = env::keccak256_array(&pubkey);
                 let address: [u8; 20] = hash[12..32].try_into().expect("Incorrect length"); // take last 20 bytes
                 EcrecoverOutput { address }
-            },
+            }
             None => EcrecoverOutput { address: [0; 20] },
         }
     }
@@ -142,20 +158,20 @@ impl Counter {
 
     pub fn domain_separator(&self) -> HashOutput {
         let hash = _domain_separator();
-        HashOutput{ hash: hash }
-    } 
+        HashOutput { hash: hash }
+    }
 
     pub fn hash_linking_accounts(&self, linking_accounts: LinkingAccounts) -> HashOutput {
         let hash = _hash_linking_accounts(linking_accounts);
-        HashOutput{ hash: hash }
+        HashOutput { hash: hash }
     }
 
     pub fn hash_linking_account(&self, linking_account: LinkingAccount) -> HashOutput {
         let hash = _hash_linking_account(linking_account);
-        HashOutput{ hash: hash }
+        HashOutput { hash: hash }
     }
 
-    pub fn hash_linking_accounts_no_domain(&self, linking_accounts: LinkingAccounts ) -> HashOutput {
+    pub fn hash_linking_accounts_no_domain(&self, linking_accounts: LinkingAccounts) -> HashOutput {
         let hash = _hash_linking_accounts_no_domain(linking_accounts);
         HashOutput { hash: hash }
     }
--- integration-tests/src/main.ava.ts
+++ integration-tests/src/main.ava.ts
@@ -1,274 +1,279 @@
-import { Worker, NEAR, NearAccount } from "near-workspaces";
-import anyTest, { TestFn } from "ava";
-import * as ethers from "ethers";
+import { Worker, NEAR, NearAccount } from 'near-workspaces'
+import anyTest, { TestFn } from 'ava'
+import * as ethers from 'ethers'
 
 const test = anyTest as TestFn<{
-  worker: Worker;
-  accounts: Record<string, NearAccount>;
-}>;
+    worker: Worker
+    accounts: Record<string, NearAccount>
+}>
 
 test.beforeEach(async (t) => {
-  // Init the worker and start a Sandbox server
-  const worker = await Worker.init();
-  const root = worker.rootAccount;
+    // Init the worker and start a Sandbox server
+    const worker = await Worker.init()
+    const root = worker.rootAccount
 
-  // some test accounts
-  const alice = await root.createSubAccount("alice", {
-    initialBalance: NEAR.parse("30 N").toJSON(),
-  });
-  const contract = await root.createSubAccount("contract", {
-    initialBalance: NEAR.parse("30 N").toJSON(),
-  });
+    // some test accounts
+    const alice = await root.createSubAccount('alice', {
+        initialBalance: NEAR.parse('30 N').toJSON(),
+    })
+    const contract = await root.createSubAccount('contract', {
+        initialBalance: NEAR.parse('30 N').toJSON(),
+    })
 
-  // Get wasm file path from package.json test script in folder above
-  await contract.deploy(process.argv[2]);
+    // Get wasm file path from package.json test script in folder above
+    await contract.deploy(process.argv[2])
 
-  // Save state for test runs, it is unique for each test
-  t.context.worker = worker;
-  t.context.accounts = { contract, alice };
-});
+    // Save state for test runs, it is unique for each test
+    t.context.worker = worker
+    t.context.accounts = { contract, alice }
+})
 
 test.afterEach(async (t) => {
-  // Stop Sandbox server
-  await t.context.worker.tearDown().catch((error) => {
-    console.log("Failed to stop the Sandbox:", error);
-  });
-});
-
-test("calculates ecrecover for personal_sign", async (t) => {
-  // ARRANGE =======================
-
-  const message = "Hello, World!";
-
-  const wallet = ethers.Wallet.createRandom();
-  const signature = await wallet.signMessage(message);
-
-  const prefixedMessage = "\x19Ethereum Signed Message:\n" + message.length + message;
-  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66); // remove 0x
-
-  const sig = signature.slice(2, 130); // first 64 bytes without 0x
-  const v = signature.slice(130, 132); // last 1 byte
-
-  // Transform yellow paper V from 27/28 to 0/1
-  // More info:
-  // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
-  // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
-  const compatibleV = parseInt('0x' + v) - 27;
-
-  const expectedAddress = wallet.address.toLowerCase();
-
-  // ACT ============================
-
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("eth_ecrecover", {
-    data: {
-      m: hash,
-      sig: sig,
-      v: compatibleV,
-      mc: false // ToDo: check correctness
-    }
-  });
-
-  const receivedAddress = '0x' + result.address.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedAddress, expectedAddress);
+    // Stop Sandbox server
+    await t.context.worker.tearDown().catch((error) => {
+        console.log('Failed to stop the Sandbox:', error)
+    })
 })
 
-test("calculates ecrecover for given signature", async (t) => {
-  // ARRANGE =======================
+test('calculates ecrecover for personal_sign', async (t) => {
+    // ARRANGE =======================
 
-  const message = "Example `personal_sign` message";
-  const signature = "0xb6483ed445dbd979650106011bc1c6e6ef255c84404df7729bdaf8bda1ae428b19c13e1447bc3fdf618b351c951d992ee236b462dc3e8f9a6fd8dc952aa18ebe1b";
-  const address = "0xaAF9E9Ce86D3f85ee15797B996c33eB720b185c0";
-  
-  const prefixedMessage = "\x19Ethereum Signed Message:\n" + message.length + message;
-  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66); // remove 0x
+    const message = 'Hello, World!'
 
-  const sig = signature.slice(2, 130); // first 64 bytes without 0x
-  const v = signature.slice(130, 132); // last 1 byte
+    const wallet = ethers.Wallet.createRandom()
+    const signature = await wallet.signMessage(message)
 
-  // Transform yellow paper V from 27/28 to 0/1
-  // More info:
-  // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
-  // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
-  const compatibleV = parseInt('0x' + v) - 27;
-  
-  const expectedAddress = address.toLowerCase();
+    const prefixedMessage = '\x19Ethereum Signed Message:\n' + message.length + message
+    const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66) // remove 0x
 
-  // ACT ============================
+    const sig = signature.slice(2, 130) // first 64 bytes without 0x
+    const v = signature.slice(130, 132) // last 1 byte
 
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("eth_ecrecover", {
-    data: {
-      m: hash,
-      sig: sig,
-      v: compatibleV,
-      mc: false // ToDo: check correctness
-    }
-  });
+    // Transform yellow paper V from 27/28 to 0/1
+    // More info:
+    // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
+    // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
+    const compatibleV = parseInt('0x' + v) - 27
 
-  const receivedAddress = '0x' + result.address.toLowerCase();
+    const expectedAddress = wallet.address.toLowerCase()
 
-  // ASSERT =========================
+    // ACT ============================
 
-  t.is(receivedAddress, expectedAddress);
-});
-
-
-test("returns hash of domain separator", async (t) => {
-  // ARRANGE =======================
-  const expectedHash = "0xab52a3fea07fbe37530706c740a2d8eae68a2eba9321a74f55b7139d4bfbae2a";
-
-  // ACT ============================
-
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("domain_separator", { });
-
-  const receivedHash = '0x' + result.hash.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedHash, expectedHash);
-})
-
-test("returns hash of LinkingAccounts without domain", async (t) => {
-  // ARRANGE =======================
-  const expectedHash = "0xe8ab30cda6d2d9fc870cf80858f49d1ba4f42b944464af3b6ae965bd3b1fede3";
-
-  // ACT ============================
-
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("hash_linking_accounts_no_domain", {
-    linking_accounts: {
-      account_a: {
-        origin_id: "origin_id",
-        account_id: "account_id"
-      },
-      account_b: {
-        origin_id: "origin_id",
-        account_id: "account_id"
-      },
-    }
-  });
-
-  const receivedHash = '0x' + result.hash.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedHash, expectedHash);
-})
-
-test("hashes EIP712 structure: LinkingAccount", async (t) => {
-  // ARRANGE =======================
-  const expectedHash = "0x69ac5e36842b01f7ca59e0931244b4b80c4d100d5f56d5f0794aa1ba59d52a03";
-
-  // ACT ============================
-
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("hash_linking_account", {
-    linking_account: {
-      origin_id: "origin_id",
-      account_id: "account_id"
-    }
-  });
-
-  const receivedHash = '0x' + result.hash.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedHash, expectedHash);
-})
-
-
-test("hashes EIP712 structure: LinkingAccounts", async (t) => {
-  // ARRANGE =======================
-  const expectedHash = "0x038bea5560ff27c731d9280c349c491e8bef260545740453d9b3b8d7c6b59d8d";
-
-  // ACT ============================
-
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("hash_linking_accounts", {
-    linking_accounts: {
-      account_a: {
-        origin_id: "origin_id",
-        account_id: "account_id"
-      },
-      account_b: {
-        origin_id: "origin_id",
-        account_id: "account_id"
-      },
-    }
-  });
-
-  const receivedHash = '0x' + result.hash.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedHash, expectedHash);
-})
-
-test("calculates ecrecover for EIP712 signature", async (t) => {
-  const data = {
-    types: {
-        LinkingAccounts: [
-            { name: "account_a", type: "LinkingAccount" },
-            { name: "account_b", type: "LinkingAccount" },
-        ],
-        LinkingAccount: [
-            { name: "origin_id", type: "string" },
-            { name: "account_id", type: "string" },
-        ],
-    },
-    domain: {
-        name: "Connected Accounts",
-        version: "1",
-        chainId: 5,
-        verifyingContract: "0x0000000000000000000000000000000000000000", // The Ethereum address of the contract that will verify the signature (accessible via this)
-    },
-    primaryType: "LinkingAccounts",
-    message: {
-        account_a: {
-            origin_id: "origin_id",
-            account_id: "account_id"
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('eth_ecrecover', {
+        data: {
+            m: hash,
+            sig: sig,
+            v: compatibleV,
+            mc: false, // ToDo: check correctness
         },
-        account_b: {
-            origin_id: "origin_id",
-            account_id: "account_id"
+    })
+
+    const receivedAddress = '0x' + result.address.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedAddress, expectedAddress)
+})
+
+test('calculates ecrecover for given signature', async (t) => {
+    // ARRANGE =======================
+
+    const message = 'Example `personal_sign` message'
+    const signature =
+        '0xb6483ed445dbd979650106011bc1c6e6ef255c84404df7729bdaf8bda1ae428b19c13e1447bc3fdf618b351c951d992ee236b462dc3e8f9a6fd8dc952aa18ebe1b'
+    const address = '0xaAF9E9Ce86D3f85ee15797B996c33eB720b185c0'
+
+    const prefixedMessage = '\x19Ethereum Signed Message:\n' + message.length + message
+    const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(prefixedMessage)).slice(2, 66) // remove 0x
+
+    const sig = signature.slice(2, 130) // first 64 bytes without 0x
+    const v = signature.slice(130, 132) // last 1 byte
+
+    // Transform yellow paper V from 27/28 to 0/1
+    // More info:
+    // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
+    // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
+    const compatibleV = parseInt('0x' + v) - 27
+
+    const expectedAddress = address.toLowerCase()
+
+    // ACT ============================
+
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('eth_ecrecover', {
+        data: {
+            m: hash,
+            sig: sig,
+            v: compatibleV,
+            mc: false, // ToDo: check correctness
+        },
+    })
+
+    const receivedAddress = '0x' + result.address.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedAddress, expectedAddress)
+})
+
+test('returns hash of domain separator', async (t) => {
+    // ARRANGE =======================
+    const expectedHash = '0xab52a3fea07fbe37530706c740a2d8eae68a2eba9321a74f55b7139d4bfbae2a'
+
+    // ACT ============================
+
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('domain_separator', {})
+
+    const receivedHash = '0x' + result.hash.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedHash, expectedHash)
+})
+
+test('returns hash of LinkingAccounts without domain', async (t) => {
+    // ARRANGE =======================
+    const expectedHash = '0x1c5301a107c19f266dfc0ea7727a368fb01a0fd1f61c433edc1d53ed6919d86c'
+
+    // ACT ============================
+
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('hash_linking_accounts_no_domain', {
+        linking_accounts: {
+            account_a: {
+                origin_id: 'origin_id',
+                account_id: 'account_id',
+            },
+            account_b: {
+                origin_id: 'origin_id',
+                account_id: 'account_id',
+            },
+            statement:
+                'I confirm that I am the owner of Account A and Account B and I want to link them in the Connected Accounts service.',
+        },
+    })
+
+    const receivedHash = '0x' + result.hash.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedHash, expectedHash)
+})
+
+test('hashes EIP712 structure: LinkingAccount', async (t) => {
+    // ARRANGE =======================
+    const expectedHash = '0x69ac5e36842b01f7ca59e0931244b4b80c4d100d5f56d5f0794aa1ba59d52a03'
+
+    // ACT ============================
+
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('hash_linking_account', {
+        linking_account: {
+            origin_id: 'origin_id',
+            account_id: 'account_id',
+        },
+    })
+
+    const receivedHash = '0x' + result.hash.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedHash, expectedHash)
+})
+
+test('hashes EIP712 structure: LinkingAccounts', async (t) => {
+    // ARRANGE =======================
+    const expectedHash = '0x7a29024e0725d369f071f440d1865857bcdc40be1568de9e04f02f62030908d7'
+
+    // ACT ============================
+
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('hash_linking_accounts', {
+        linking_accounts: {
+            account_a: {
+                origin_id: 'origin_id',
+                account_id: 'account_id',
+            },
+            account_b: {
+                origin_id: 'origin_id',
+                account_id: 'account_id',
+            },
+            statement:
+                'I confirm that I am the owner of Account A and Account B and I want to link them in the Connected Accounts service.',
+        },
+    })
+
+    const receivedHash = '0x' + result.hash.toLowerCase()
+
+    // ASSERT =========================
+
+    t.is(receivedHash, expectedHash)
+})
+
+test('calculates ecrecover for EIP712 signature', async (t) => {
+    const wallet = ethers.Wallet.createRandom()
+    const expectedAddress = wallet.address.toLowerCase()
+
+    const data = {
+        types: {
+            LinkingAccounts: [
+                { name: 'account_a', type: 'LinkingAccount' },
+                { name: 'account_b', type: 'LinkingAccount' },
+                { name: 'statement', type: 'string' },
+            ],
+            LinkingAccount: [
+                { name: 'origin_id', type: 'string' },
+                { name: 'account_id', type: 'string' },
+            ],
+        },
+        domain: {
+            name: 'Connected Accounts',
+            version: '1',
+            chainId: 5,
+            verifyingContract: '0x0000000000000000000000000000000000000000', // The Ethereum address of the contract that will verify the signature (accessible via this)
+        },
+        primaryType: 'LinkingAccounts',
+        message: {
+            account_a: {
+                origin_id: 'near/testnet',
+                account_id: 'alice.test.near',
+            },
+            account_b: {
+                origin_id: 'ethereum',
+                account_id: expectedAddress,
+            },
+            statement:
+                'I confirm that I am the owner of Account A and Account B and I want to link them in the Connected Accounts service.',
         },
     }
-  };
+    const signature = await wallet._signTypedData(data.domain, data.types, data.message)
 
-  const wallet = ethers.Wallet.createRandom();
-  const signature = await wallet._signTypedData(data.domain, data.types, data.message);
+    const sig = signature.slice(2, 130) // first 64 bytes without 0x
+    const v = signature.slice(130, 132) // last 1 byte
 
-  const sig = signature.slice(2, 130); // first 64 bytes without 0x
-  const v = signature.slice(130, 132); // last 1 byte
+    // Transform yellow paper V from 27/28 to 0/1
+    // More info:
+    // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
+    // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
+    const compatibleV = parseInt('0x' + v) - 27
 
-  // Transform yellow paper V from 27/28 to 0/1
-  // More info:
-  // https://stackoverflow.com/questions/49085737/geth-ecrecover-invalid-signature-recovery-id
-  // https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L459
-  const compatibleV = parseInt('0x' + v) - 27;
+    // ACT ============================
 
-  const expectedAddress = wallet.address.toLowerCase();
+    const { contract } = t.context.accounts
+    const result: any = await contract.view('eth_verify_eip712', {
+        linking_accounts: data.message,
+        signature: {
+            sig: sig,
+            v: compatibleV,
+            mc: false, // ToDo: check correctness
+        },
+    })
 
-  // ACT ============================
+    const receivedAddress = '0x' + result.address.toLowerCase()
 
-  const { contract } = t.context.accounts;
-  const result: any = await contract.view("eth_verify_eip712", {
-    linking_accounts: data.message,
-    signature: {
-      sig: sig,
-      v: compatibleV,
-      mc: false // ToDo: check correctness
-    }
-  });
+    // ASSERT =========================
 
-  const receivedAddress = '0x' + result.address.toLowerCase();
-
-  // ASSERT =========================
-
-  t.is(receivedAddress, expectedAddress);
+    t.is(receivedAddress, expectedAddress)
 })
